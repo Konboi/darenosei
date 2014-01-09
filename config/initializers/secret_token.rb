e64ff2d8ceb7dbc0eee63ec939a83fdb5657bf27
Konboi <ryosuke.yabuki@gmail.com>
2014-01-09 18:13:04 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Darenosei::Application.config.secret_key_base = '4c0765c3cf582e368cd1dd23f6e09a9e0869078ea9187dc7e67463cb31195bbb5413019ba21aa0b83deb72ad820bbfa204cc391e786cd0c7d1ffb483166d8abb'
